701cfb6459a5ce4ff3c10ccc6974c2f897818e8c
RuiRui.b <89136205+1528473765@users.noreply.github.com>
2022-08-19 08:05:44 +0800

    Update shuaquantu.js

--- js/shuaquantu.js
+++ js/shuaquantu.js
@@ -2,8 +2,9 @@ var obj = JSON.parse($response.body);
 var obj = {
   "status": 1,
   "info": {
-  "is_timeout": false,
+  "username": "666",
   "group": "终身会员",
+  "is_timeout": false,
   }
 };
 $done({body: JSON.stringify(obj)});
