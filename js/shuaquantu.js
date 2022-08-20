3c76234a9a1ec96c4644e84a6232153f71c4a6d1
RuiRui.b <89136205+1528473765@users.noreply.github.com>
2022-08-20 22:20:31 +0800

    Update shuaquantu.js

--- js/shuaquantu.js
+++ js/shuaquantu.js
@@ -1,9 +1,23 @@
 var obj = JSON.parse($response.body);
 var obj = {
-  "status": 1,
-  "info": {
-    "group": "终身会员",
-    "is_timeout": false,
+  "status" : 1,
+  "info" : {
+    "is_timeout" : false,
+    "uuids" : "",
+    "nickName" : "",
+    "alipayName" : "",
+    "inviteCode_state" : 0,
+    "inviteUserBuyNum" : 0,
+    "inviteMoney" : 0,
+    "group" : "终身会员",
+    "inviteCode" : "",
+    "is_agent" : "0",
+    "m3_num" : "0",
+    "inviteBili" : 0,
+    "end_time" : "无限期",
+    "inviteUserNum" : 0,
+    "alipayId" : "",
+    "isGF" : 0,
   }
 }
 $done({body: JSON.stringify(obj)});
